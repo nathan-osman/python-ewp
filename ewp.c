1834b4b57f0e4d2894fe046ed1fe984e649ac2eb
Nathan Osman <nathan@quickmediasolutions.com>
2016-04-13 22:07:50 -0700

    Added implementation of encrypt() method.

--- ewp.c
+++ ewp.c
@@ -25,16 +25,25 @@
 #include <Python.h>
 #include <bytesobject.h>
 #include <openssl/bio.h>
+#include <openssl/evp.h>
 #include <openssl/opensslv.h>
 #include <openssl/pem.h>
 #include <openssl/pkcs7.h>
 #include <openssl/ssl.h>
+#include <openssl/x509.h>
 
 /* There are a few subtle differences between Python 2.x and 3.x */
 #if PY_MAJOR_VERSION >= 3
 #define IS_PY3K
 #endif
 
+/**
+ * Sign message with a private key and certificate.
+ * @param key filename of PEM-encoded private key
+ * @param cert filename of PEM-encoded X509 certificate
+ * @param input message to sign
+ * @return message signature
+ */
 static PyObject *ewp_sign(PyObject *self, PyObject *args)
 {
     const char *keyFn, *certFn, *input;
@@ -146,8 +155,120 @@ end:
     return signature;
 }
 
+/**
+ * Encrypt message with a certificate.
+ * @param cert filename of PEM-encoded X509 certificate
+ * @param input message to encrypt
+ * @return encrypted message
+ */
+static PyObject *ewp_encrypt(PyObject *self, PyObject *args)
+{
+    const char *certFn, *input;
+    int inputLen;
+    BIO *bioCert = NULL;
+    X509 *x509 = NULL;
+    STACK_OF(X509) *skX509 = NULL;
+    BIO *bioInput = NULL;
+    PKCS7 *p7 = NULL;
+    BIO *bioOutput = NULL;
+    char *output;
+    long outputLen;
+    PyObject *ciphertext = NULL;
+
+    if (!PyArg_ParseTuple(args, "ss#", &certFn, &input, &inputLen)) {
+        return NULL;
+    }
+
+    /* Read the contents of the certificate */
+    bioCert = BIO_new_file(certFn, "r");
+    if (!bioCert) {
+        PyErr_SetString(PyExc_IOError, "unable to open certificate");
+        goto end;
+    }
+
+    /* Attempt to load the certificate as a PEM-encoded file */
+    x509 = PEM_read_bio_X509(bioCert, NULL, NULL, NULL);
+    if (!x509) {
+        PyErr_SetString(PyExc_IOError, "unable to read certificate");
+        goto end;
+    }
+
+    /* Create an empty stack of X509 certificates */
+    skX509 = sk_X509_new_null();
+    if (!skX509) {
+        PyErr_SetString(PyExc_RuntimeError, "unable to create X509 stack");
+        goto end;
+    }
+
+    /* Push the certificate onto the stack */
+    if (!sk_X509_push(skX509, x509)) {
+        PyErr_SetString(PyExc_RuntimeError, "unable to add to X509 stack");
+        goto end;
+    }
+
+    /* Create a BIO from the input data in memory */
+    bioInput = BIO_new_mem_buf((void *)input, inputLen);
+    if (!bioInput) {
+        PyErr_SetString(PyExc_RuntimeError, "unable to create input BIO");
+        goto end;
+    }
+
+    /* Encrypt the input data */
+    p7 = PKCS7_encrypt(skX509, bioInput, EVP_des_ede3_cbc(), PKCS7_BINARY);
+    if (!p7) {
+        PyErr_SetString(PyExc_RuntimeError, "unable to encrypt data");
+        goto end;
+    }
+
+    /* Create a BIO for writing the ciphertext */
+    bioOutput = BIO_new(BIO_s_mem());
+    if (!bioOutput) {
+        PyErr_SetString(PyExc_RuntimeError, "unable to create output BIO");
+        goto end;
+    }
+
+    /* Write the ciphertext to the BIO */
+    if (!i2d_PKCS7_bio(bioOutput, p7)) {
+        PyErr_SetString(PyExc_RuntimeError, "unable to write ciphertext");
+        goto end;
+    }
+
+    /* Obtain a pointer to the data and set the return value */
+    outputLen = BIO_get_mem_data(bioOutput, &output);
+    ciphertext = PyBytes_FromStringAndSize(output, outputLen);
+
+end:
+
+    if (bioOutput) {
+        BIO_free(bioOutput);
+    }
+
+    if (p7) {
+        PKCS7_free(p7);
+    }
+
+    if (bioInput) {
+        BIO_free(bioInput);
+    }
+
+    if (skX509) {
+        sk_X509_free(skX509);
+    }
+
+    if (x509) {
+        X509_free(x509);
+    }
+
+    if (bioCert) {
+        BIO_free(bioCert);
+    }
+
+    return ciphertext;
+}
+
 static PyMethodDef EWPMethods[] = {
     { "sign", ewp_sign, METH_VARARGS, "Sign input" },
+    { "encrypt", ewp_encrypt, METH_VARARGS, "Encrypt input" },
     { NULL, NULL, 0, NULL }
 };
 
